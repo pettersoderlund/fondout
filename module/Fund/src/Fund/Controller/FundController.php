e68f398297b8332c450ed1aace05261be6466ecd
Petter Söderlund <pettersoderlund@gmail.com>
2014-07-15 15:00:54 +0200

    Added pagination to funds fds fixes #4

--- module/Fund/src/Fund/Controller/FundController.php
+++ module/Fund/src/Fund/Controller/FundController.php
@@ -4,6 +4,10 @@ namespace Fund\Controller;
 use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\ViewModel;
 
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use Zend\Paginator\Paginator;
+
 class FundController extends AbstractRestfulController
 {
     public function getList()
@@ -12,11 +16,19 @@ class FundController extends AbstractRestfulController
             ->getServiceLocator()
             ->get('Doctrine\ORM\EntityManager');
 
-        return new ViewModel(
-            array(
-             'funds' => $objectManager->getRepository('Fund\Entity\Fund')->findAll()
-            )
-        );
+        $view = new ViewModel();
+
+        $repository = $objectManager->getRepository('Fund\Entity\Fund');
+        $adapter = new DoctrineAdapter(new ORMPaginator($repository->createQueryBuilder('fund')));
+
+        $paginator = new Paginator($adapter);
+        $paginator->setCurrentPageNumber((int)$this->params()->fromQuery('page', 1));
+        
+        // set the number of items per page to 10
+        $paginator->setItemCountPerPage(10);
+        $view->setVariable('funds', $paginator);
+
+        return $view;
     }
 
     public function get($id)
