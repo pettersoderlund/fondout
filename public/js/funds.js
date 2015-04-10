20e09f42ad5185a2d164b152346645a008197d99
Petter Söderlund <pettersoderlund@gmail.com>
2015-04-10 10:50:16 +0200

    First design round fund search

--- public/js/funds.js
+++ public/js/funds.js
@@ -1,5 +1,6 @@
 'use strict';
 $(document).ready(function() {
+
   $('#filter-company').selectize({
       plugins: ['remove_button'],
       sortField: 'text'
@@ -18,4 +19,21 @@ $(document).ready(function() {
     html: true,
     trigger: 'hover'
   });
+});
+
+/* Denna funktion för sa sort by baren blir fixed nar man skrollar ner. */
+$(document).ready(function () {
+  var menu = $('.sort-menu');
+  var origOffsetY = menu.offset().top;
+
+  function scroll() {
+      if ($(window).scrollTop() >= origOffsetY) {
+          $('.sort-menu').addClass('navbar-fixed-top');
+          $('.content').addClass('menu-padding');
+      } else {
+          $('.sort-menu').removeClass('navbar-fixed-top');
+          $('.content').removeClass('menu-padding');
+      }
+  }
+  document.onscroll = scroll;
 });
