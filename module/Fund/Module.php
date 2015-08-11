4fddbad6baa8592aa003ed8895bb73518e284d8a
Petter Söderlund <pettersoderlund@gmail.com>
2015-08-11 12:05:18 +0200

    Addad consule route help descriptions

--- module/Fund/Module.php
+++ module/Fund/Module.php
@@ -4,11 +4,16 @@ namespace Fund;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
-
 use Fund\Service\FundService;
 use Fund\Service\ConsoleService;
+//use Zend\ModuleManager\Feature\ConsoleBannerProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+use Zend\Console\Adapter\AdapterInterface as Console;
 
-class Module implements AutoloaderProviderInterface
+
+class Module implements AutoloaderProviderInterface,
+                        //ConsoleBannerProviderInterface,
+                        ConsoleUsageProviderInterface
 {
     public function getAutoloaderConfig()
     {
@@ -53,6 +58,31 @@ class Module implements AutoloaderProviderInterface
     {
         return include __DIR__ . '/config/module.config.php';
     }
+
+    /*
+    public function getConsoleBanner(Console $console)
+    {
+      return 'Fund module';
+    }
+    */
+
+    public function getConsoleUsage(Console $console)
+    {
+        return array(
+            'update fund-measures'  => 'Updates calculated fund measures of nbr of companies, nav and category stats.',
+            'create sitemap'        => 'Prints a sitemap to console. Put in a xml file!',
+            'map sharecompanies [--verbose|-v] <csvfileIsinToSharecompany>' => 'Import file to map share isin to share companies.',
+            'add companyaccusations [--verbose|-v] <companyAccusations>' => 'Import file to map accusations to share companies.',
+            'add sc-marketcap <file>' => 'Import file containing share company market caps in SEK.',
+            'add fund --date= [--exchangerate=] [--doubleholdings] [--smallbatch] <file>' => 'Import fund',
+            array('--date', 'Date on format m/d/Y'),
+            array('--exchangerate', '(optional) Exchange rate for the corresponding date if currency is other than SEK.'),
+            array('--smallbatch', 'Use a smaller batch to aviod duplicates in alias.' ),
+            array('--doubleholdings', 'If the fund has multiples of the same share use this flag. Make sure the earlier fund entry for this instance is empty.' ),
+
+          );
+    }
+
 
     public function onBootstrap(MvcEvent $e)
     {
